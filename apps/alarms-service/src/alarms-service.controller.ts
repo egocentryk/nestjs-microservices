81a78ce82e57ea05fbd457d2d7138b9d27f23f84
egocentryk <lukasz.skowron@gmail.com>
2025-03-04 11:21:21 +0100

    feat(): alarm classifier and notifications services implemented

--- apps/alarms-service/src/alarms-service.controller.ts
+++ apps/alarms-service/src/alarms-service.controller.ts
@@ -1,16 +1,40 @@
-import { Controller, Get, Logger } from '@nestjs/common'
-import { EventPattern, Payload } from '@nestjs/microservices'
+import { Controller, Get, Inject, Logger } from '@nestjs/common'
+import { ClientProxy, EventPattern, Payload } from '@nestjs/microservices'
+import { MESSAGE_BROKER } from './constants'
+import { lastValueFrom } from 'rxjs'
 
 @Controller()
 export class AlarmsServiceController {
   private readonly logger = new Logger(AlarmsServiceController.name)
 
+  constructor(
+    @Inject(MESSAGE_BROKER)
+    private readonly messageBroker: ClientProxy,
+  ) {}
+
   @EventPattern('alarm.created')
-  create(@Payload() data: unknown) {
+  async create(@Payload() data: { name: string; buildingId: number }) {
     this.logger.debug(
       `Received new "alarm.created" event with the following data: ${JSON.stringify(
         data,
       )}`,
     )
+
+    const alarmClassification = await lastValueFrom(
+      this.messageBroker.send('alarm.classify', data),
+    )
+
+    this.logger.debug(
+      `Alarm "${data.name}" classified as ${alarmClassification.category}`,
+    )
+
+    const notify$ = this.messageBroker.emit('notification.send', {
+      alarm: data,
+      category: alarmClassification.category,
+    })
+
+    await lastValueFrom(notify$)
+
+    this.logger.debug(`Dispatched "notification.send" event`)
   }
 }
